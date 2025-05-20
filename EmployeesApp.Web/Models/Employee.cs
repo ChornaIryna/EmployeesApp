b2aeb7ab8c93d88dd534746da24fbec1407e002e
Iryna <iryna.chorna@hotmail.com>
2025-05-20 11:59:49 +0200

    Added View Models: Create and Index

--- EmployeesApp.Web/Models/Employee.cs
+++ EmployeesApp.Web/Models/Employee.cs
@@ -1,20 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Runtime.CompilerServices;
+﻿namespace EmployeesApp.Web.Models;
 
-namespace EmployeesApp.Web.Models
+public class Employee
 {
-    public class Employee
-    {
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        [Required(ErrorMessage = "You must specify a name")]
-        [SuspiciousPerson("Pontus Wittenmark", ErrorMessage = "Suspicious person detected!")]
-        public string Name { get; set; } = null!;
+    public string Name { get; set; } = null!;
 
-        [Display(Name = "E-mail")]
-        [EmailAddress(ErrorMessage = "Invalid e-mail address")]
-        [Required(ErrorMessage = "You must specify an e-mail address")]
-        public string Email { get; set; } = null!;
+    public string Email { get; set; } = null!;
 
-    }
 }
